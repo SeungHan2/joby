80f5b112f0b176a5a519591f3e05b9ca44c4e7a2
SeungHan2 <bigbrain210@gmail.com>
2025-10-02 23:54:14 +0900

    Update main.py with Telegram report code

--- main.py
+++ main.py
@@ -1,11 +1,2 @@
-import yfinance as yf
-from telegram import Bot
+print("Hello, world!")
 
-TOKEN = "텔레그램 봇 토큰"
-CHAT_ID = "텔레그램 채팅 ID"
-bot = Bot(TOKEN)
-
-sp500 = yf.Ticker("^GSPC")
-price = sp500.history(period="1d")['Close'][0]
-
-bot.send_message(chat_id=CHAT_ID, text=f"오늘 S&P500 종가: {price:.2f}")
